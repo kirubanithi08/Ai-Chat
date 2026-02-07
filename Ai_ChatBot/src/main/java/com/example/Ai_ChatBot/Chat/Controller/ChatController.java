9f2ba3507feb4ec17e8174e452aa654e82c1bfb9
Kiruba Nithi <kirubanithi812@gmail.com>
2026-02-07 16:51:33 +0530

    chat service and dto

--- Ai_ChatBot/src/main/java/com/example/Ai_ChatBot/Chat/Controller/ChatController.java
+++ Ai_ChatBot/src/main/java/com/example/Ai_ChatBot/Chat/Controller/ChatController.java
@@ -3,6 +3,7 @@ package com.example.Ai_ChatBot.Chat.Controller;
 import com.example.Ai_ChatBot.Chat.Dto.ChatRequest;
 import com.example.Ai_ChatBot.Chat.Dto.ChatResponse;
 import com.example.Ai_ChatBot.Chat.Service.ChatService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,7 @@ public class ChatController {
 
     @PostMapping
     public ResponseEntity<ChatResponse> chat(
-            @RequestBody ChatRequest request
+            @Valid @RequestBody ChatRequest request
     ) {
         return ResponseEntity.ok(chatService.chat(request));
     }
--- Ai_ChatBot/src/main/java/com/example/Ai_ChatBot/Chat/Dto/ChatRequest.java
+++ Ai_ChatBot/src/main/java/com/example/Ai_ChatBot/Chat/Dto/ChatRequest.java
@@ -1,11 +1,15 @@
 package com.example.Ai_ChatBot.Chat.Dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ChatRequest {
 
     private Long sessionId;
+    @NotBlank
+    @Size(max = 2000)
     private String message;
 }
 
