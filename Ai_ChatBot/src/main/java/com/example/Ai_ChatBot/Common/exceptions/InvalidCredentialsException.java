79c0f48f98583bf00f719a14c0c16c47d4e77640
Kiruba Nithi <kirubanithi812@gmail.com>
2026-02-13 22:33:53 +0530

    custom exception

---
+++ Ai_ChatBot/src/main/java/com/example/Ai_ChatBot/Common/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.example.Ai_ChatBot.Common.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
+
